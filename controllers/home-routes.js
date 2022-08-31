7dcc8285d2406d2302cbfbb86be2efcfd736ac5a
Katie <katiebutler37@gmail.com>
2022-08-31 10:24:04 -0400

    commented out home-routes attributes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -5,23 +5,23 @@ const { Post, User, Comment } = require('../models');
 router.get('/', (req, res) => {
     Post.findAll({
         attributes: [
-          'id',
-          'title',
-          'post_content',
-          'created_at'
+          // 'id',
+          // 'title',
+          // 'post_content',
+          // 'created_at'
         ],
         include: [
           {
             model: Comment,
-            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            // attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
             include: {
               model: User,
-              attributes: ['username']
+              // attributes: ['username']
             }
           },
           {
             model: User,
-            attributes: ['username']
+            // attributes: ['username']
           }
         ]
       })
